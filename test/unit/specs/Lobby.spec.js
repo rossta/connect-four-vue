d1011fde07e01fb47aa390eeba415471aab9efdf
Ross Kaffenberger <rosskaff@gmail.com>
2017-12-14 08:57:19 -0500

    Add vue-test-urlts to project

--- test/unit/specs/Lobby.spec.js
+++ test/unit/specs/Lobby.spec.js
@@ -1,11 +1,24 @@
 import Vue from 'vue';
+import { createLocalVue, shallow } from 'vue-test-utils';
+
 import Lobby from '@/components/Lobby';
 
+const push = {
+  join() { return this; },
+  receive() { return this; },
+};
+const phoenix = {
+  channel() { return push; }
+};
+
 describe('Lobby.vue', () => {
+  let phoenix, mocks;
+  beforeEach(() => {
+    mocks = { $phoenix: phoenix };
+  });
+
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(Lobby);
-    const vm = new Constructor().$mount();
-    expect(vm.$el.querySelector('.hello h1').textContent)
-    .toEqual('Welcome to Connect Four');
+    const wrapper = shallow(Lobby, phoenix) ;
+    expect(wrapper.find('.hello h1').textContent).toEqual('Welcome to Connect Four');
   });
 });
